555d663d01bb7d625f01ea16d7d93b1a688c0c8a
Anthony LAULOM <alaulom@e3r6p16.42.fr>
2016-01-13 13:04:19 +0100

    gg

---
+++ ft_strdup.s
@@ -0,0 +1,36 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_strdup.s                                        :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: alaulom <marvin@42.fr>                     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2016/01/13 12:48:09 by alaulom           #+#    #+#              #
+#    Updated: 2016/01/13 13:04:06 by alaulom          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+;char		*ft_strdup(const char *s1);
+
+global _ft_strdup
+extern _ft_strlen _ft_memcpy _malloc
+
+section .text
+
+_ft_strdup:
+		enter		0, 0
+		cmp			rdi, 0
+		je			_ret
+		call		_ft_strlen
+		push		rdi
+		push		rax
+		mov			rdi, rax
+		call		_malloc
+		mov			rdi, rax
+		pop			rdx
+		pop			rsi
+		call		_ft_memcpy
+
+_ret:
+		leave
+		ret
