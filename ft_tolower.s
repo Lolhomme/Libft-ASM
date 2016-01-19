779a35c189b57da023f4bdd03d5aa54c85f5410c
Anthony LAULOM <alaulom@e3r7p19.42.fr>
2016-01-19 14:42:46 +0100

    gg

---
+++ ft_tolower.s
@@ -0,0 +1,29 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_tolower.s                                       :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: alaulom <marvin@42.fr>                     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2016/01/06 16:24:27 by alaulom           #+#    #+#              #
+#    Updated: 2016/01/19 14:42:30 by alaulom          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+;int		ft_tolower(int c);
+
+global _ft_tolower
+
+section .text
+
+_ft_tolower:
+		if1:	cmp		rdi, 'A'
+				jge		endif1
+				cmp		rdi, 'Z'
+				jle		endif1
+				add		rdi, 32
+		endif1:
+
+_ret:
+		mov		rax, rdi
+		ret
